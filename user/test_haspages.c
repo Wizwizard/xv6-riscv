a02d382642a9f5f5a07ee3f7ecb25a76d86bd46e
BingxingWang <303704668@qq.com>
2022-10-31 17:51:02 -0500

    fix has_pages

--- user/test_haspages.c
+++ user/test_haspages.c
@@ -12,20 +12,20 @@ main(int argc, char *argv[])
   char * str;
   str = (char *) malloc (4096);
 
-  has_pages(p_id);
+  haspages(p_id);
   // malloc another 4096
   char * str2;
   str2 = (char*) malloc (4096);
 
-  has_pages(p_id);
+  haspages(p_id);
 
   // free first
   free(str);
-  has_pages(p_id);
+  haspages(p_id);
 
   // free next
   free(str2);
-  has_pages(p_id);
+  haspages(p_id);
 
   exit(0);
 }
